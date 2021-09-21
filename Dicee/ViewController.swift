a3ad33c84caccb90a9f3a62447e6235898eadb4a
Filipe <fil.desouza@gmail.com>
2021-09-21 17:04:17 -0300

    Primeira versão

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
     }
 
-
+    @IBAction func btRodarDados(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceOne.image = diceArray.randomElement()
+        
+        diceTwo.image = diceArray[Int.random(in: 0...5)]
+        
+        
+    }
+    
 }
 
